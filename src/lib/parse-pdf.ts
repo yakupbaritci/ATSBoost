95cdd686d8e57c008655322f82170d1e899e825d
Yakup <yakupbaritci@gmail.com>
2026-02-04 20:16:09 +0300

    Fix: Polyfill DOMMatrix for pdf-parse in Node environment

--- src/lib/parse-pdf.ts
+++ src/lib/parse-pdf.ts
@@ -1,3 +1,17 @@
+// Polyfill for Node.js environment (fixes DOMMatrix is not defined)
+if (typeof Promise.withResolvers === 'undefined') {
+    // @ts-ignore
+    if (typeof window === 'undefined' && !global.DOMMatrix) {
+        // @ts-ignore
+        global.DOMMatrix = class DOMMatrix {
+            constructor() {
+                this.a = 1; this.b = 0; this.c = 0; this.d = 1; this.e = 0; this.f = 0;
+            }
+            toString() { return "matrix(1, 0, 0, 1, 0, 0)"; }
+        }
+    }
+}
+
 // @ts-ignore
 const pdf = require('pdf-parse');
 
