6dc704884246fe0530a739f4582daf7944c10593
Yakup <yakupbaritci@gmail.com>
2026-02-05 22:16:24 +0300

    Chore: Add missing UI components and font files

--- scripts/download-fonts.js
+++ scripts/download-fonts.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const path = require('path');
-const https = require('https');
 
 const fontsDir = path.join(process.cwd(), 'public', 'fonts');
 
@@ -8,31 +7,37 @@ if (!fs.existsSync(fontsDir)) {
     fs.mkdirSync(fontsDir, { recursive: true });
 }
 
+// Using Google Fonts GitHub repository raw URLs which are stable
 const fonts = {
-    "Roboto-Regular.ttf": "https://fonts.gstatic.com/s/roboto/v32/KFOmCnqEu92Fr1Mu4mxK.ttf",
-    "Roboto-Bold.ttf": "https://fonts.gstatic.com/s/roboto/v32/KFOlCnqEu92Fr1MmWUlfBBc4.ttf",
-    "Roboto-Italic.ttf": "https://fonts.gstatic.com/s/roboto/v32/KFOkCnqEu92Fr1Mu51xIIzIXKMny.ttf",
-    "Roboto-BoldItalic.ttf": "https://fonts.gstatic.com/s/roboto/v32/KFOjCnqEu92Fr1Mu51TzBhc4CsE.ttf",
-    "Merriweather-Regular.ttf": "https://fonts.gstatic.com/s/merriweather/v30/u8y7gqDvlWzRn0g2Y71bGTtX.ttf",
-    "Merriweather-Bold.ttf": "https://fonts.gstatic.com/s/merriweather/v30/u8yznqDvlWzRn0g2Y71bHqsuPWIr.ttf",
-    "Merriweather-Italic.ttf": "https://fonts.gstatic.com/s/merriweather/v30/u8yzmqDvlWzRn0g2Y71bDq8sPZwrYg.ttf",
-    "Merriweather-BoldItalic.ttf": "https://fonts.gstatic.com/s/merriweather/v30/u8ywmqDvlWzRn0g2Y71bHqsWNW82.ttf"
+    "Roboto-Regular.ttf": "https://raw.githubusercontent.com/google/fonts/main/apache/roboto/static/Roboto-Regular.ttf",
+    "Roboto-Bold.ttf": "https://raw.githubusercontent.com/google/fonts/main/apache/roboto/static/Roboto-Bold.ttf",
+    "Roboto-Italic.ttf": "https://raw.githubusercontent.com/google/fonts/main/apache/roboto/static/Roboto-Italic.ttf",
+    "Roboto-BoldItalic.ttf": "https://raw.githubusercontent.com/google/fonts/main/apache/roboto/static/Roboto-BoldItalic.ttf",
+    "Merriweather-Regular.ttf": "https://raw.githubusercontent.com/google/fonts/main/ofl/merriweather/Merriweather-Regular.ttf",
+    "Merriweather-Bold.ttf": "https://raw.githubusercontent.com/google/fonts/main/ofl/merriweather/Merriweather-Bold.ttf",
+    "Merriweather-Italic.ttf": "https://raw.githubusercontent.com/google/fonts/main/ofl/merriweather/Merriweather-Italic.ttf",
+    "Merriweather-BoldItalic.ttf": "https://raw.githubusercontent.com/google/fonts/main/ofl/merriweather/Merriweather-BoldItalic.ttf"
 };
 
-Object.entries(fonts).forEach(([filename, url]) => {
-    const dest = path.join(fontsDir, filename);
-    const file = fs.createWriteStream(dest);
+async function download() {
+    for (const [filename, url] of Object.entries(fonts)) {
+        const dest = path.join(fontsDir, filename);
+        console.log(`Downloading ${filename} from ${url}...`);
 
-    console.log(`Downloading ${filename}...`);
+        try {
+            const response = await fetch(url);
+            if (!response.ok) throw new Error(`HTTP ${response.status} - ${response.statusText}`);
 
-    https.get(url, (response) => {
-        response.pipe(file);
-        file.on('finish', () => {
-            file.close();
-            console.log(`Downloaded ${filename}`);
-        });
-    }).on('error', (err) => {
-        fs.unlink(dest, () => { }); // Delete partial file
-        console.error(`Error downloading ${filename}: ${err.message}`);
-    });
-});
+            const buffer = await response.arrayBuffer();
+            if (buffer.byteLength < 1000) {
+                console.warn(`Warning: ${filename} is surprisingly small (${buffer.byteLength} bytes). Check URL.`);
+            }
+            fs.writeFileSync(dest, Buffer.from(buffer));
+            console.log(`Downloaded ${filename} (${buffer.byteLength} bytes)`);
+        } catch (err) {
+            console.error(`Error downloading ${filename}:`, err);
+        }
+    }
+}
+
+download();
